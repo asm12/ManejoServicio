52a932c2c95324a2b5731736771a134be8efcb01
Albert <asm28@alumnes.udl.cat>
2021-04-18 22:25:39 +0200

    CustomBroadcastReceiver class completed.

--- app/src/main/java/com/android/mdw/demo/CustomBroadcastReceiver.java
+++ app/src/main/java/com/android/mdw/demo/CustomBroadcastReceiver.java
@@ -3,43 +3,50 @@ package com.android.mdw.demo;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
-import android.util.Log;
+import android.widget.Toast;
+
 
 public class CustomBroadcastReceiver extends BroadcastReceiver {
     private static final String TAG = "CustomBroadcastReceiver";
+    private Intent intent;
+    private Context context;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        final PendingResult pendingResult = goAsync();
-        Task asyncTask = new Task(pendingResult, intent);
-        asyncTask.execute();
+        this.context = context;
+        this.intent = intent;
+        checkAction();
+
     }
 
-    private static class Task extends AsyncTask<String, Integer, String> {
-        private final PendingResult pendingResult;
-        private final Intent intent;
+    private void checkAction() {
+        String action = intent.getStringExtra("action");
+        Intent toSend = new Intent(context, ElServicio.class);
+        boolean stop = false;
 
-        private Task(PendingResult pendingResult, Intent intent) {
-            this.pendingResult = pendingResult;
-            this.intent = intent;
-        }
+        if (action != null) {
+            if (action.equals(context.getString(R.string.iniciar_sonido))) {
 
-        @Override
-        protected String doInBackground(String... strings) {
-            StringBuilder sb = new StringBuilder();
-            sb.append("Action: " + intent.getAction() + "\n");
-            sb.append("URI: " + intent.toUri(Intent.URI_INTENT_SCHEME).toString() + "\n");
-            String log = sb.toString();
-            Log.d(TAG, log);
-            return log;
-        }
+                toSend.putExtra("action", context.getString(R.string.iniciar_sonido));
+                SingleToast.show(context, "Intent recibido" +
+                        "ElReceptor - Inicio reproducción Sonido", Toast.LENGTH_SHORT);
 
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            // Must call finish() so the BroadcastReceiver can be recycled.
-            pendingResult.finish();
+            } else if (action.equals(context.getString(R.string.iniciar_cancion))) {
+
+                toSend.putExtra("action", context.getString(R.string.iniciar_cancion));
+                SingleToast.show(context, "Intent recibido" +
+                        "ElReceptor - Inicio reproducción Cancion", Toast.LENGTH_SHORT);
+            } else {
+                stop = true;
+                toSend.putExtra("action", context.getString(R.string.detener));
+                SingleToast.show(context, "Intent recibido" +
+                        "ElReceptor - Detención Reproducción", Toast.LENGTH_SHORT);
+            }
+            if (stop) {
+                context.stopService(toSend);
+            } else {
+                context.startService(toSend);
+            }
         }
     }
 }
