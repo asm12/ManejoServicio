d66d8dabc94bbbd24c0387a850bf61abbcebf399
Albert <asm28@alumnes.udl.cat>
2021-04-18 22:26:05 +0200

    Added necessary controls in ElServicio

--- app/src/main/java/com/android/mdw/demo/ElServicio.java
+++ app/src/main/java/com/android/mdw/demo/ElServicio.java
@@ -8,32 +8,58 @@ import android.widget.Toast;
 
 public class ElServicio extends Service {
 
-	private MediaPlayer player;
+    private MediaPlayer soundPlayer;
+    private MediaPlayer songPlayer;
+    private Intent intent;
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	@Override
-	public void onCreate() {
-		SingleToast.show(this, getString(R.string.creaserv), Toast.LENGTH_LONG);
-		player = MediaPlayer.create(this, R.raw.train);
-		player.setLooping(true);
-	}
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public void onDestroy() {
-		SingleToast.show(this, getString(R.string.finaserv), Toast.LENGTH_LONG);
-		player.stop();
-	}
-	
-	@Override
-	public int onStartCommand(Intent intent, int flags, int startid) {
-		SingleToast.show(this, getString(R.string.iniserv), Toast.LENGTH_LONG);
-		player.start();
-		return startid;		
-	}	
+    @Override
+    public void onCreate() {
+        SingleToast.show(this, getString(R.string.creaserv), Toast.LENGTH_LONG);
+        soundPlayer = new MediaPlayer();
+        songPlayer = new MediaPlayer();
+        super.onCreate();
+    }
+
+    private void createSoundPlayer() {
+        String action = intent.getStringExtra("action");
+        if (action != null) {
+            if (action.equals(getString(R.string.iniciar_sonido)) && !soundPlayer.isPlaying()) {
+                soundPlayer = MediaPlayer.create(this, R.raw.train);
+                soundPlayer.setLooping(true);
+                soundPlayer.start();
+
+            } else if (!songPlayer.isPlaying()) {
+                songPlayer = MediaPlayer.create(this, R.raw.tank);
+                songPlayer.setLooping(true);
+                songPlayer.start();
+
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        SingleToast.show(this, getString(R.string.finaserv), Toast.LENGTH_LONG);
+        if (songPlayer.isPlaying()) {
+            songPlayer.stop();
+        }
+        if (soundPlayer.isPlaying()) {
+            soundPlayer.stop();
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startid) {
+        SingleToast.show(this, getString(R.string.iniserv), Toast.LENGTH_LONG);
+        this.intent = intent;
+        createSoundPlayer();
+        return startid;
+    }
 
 }
