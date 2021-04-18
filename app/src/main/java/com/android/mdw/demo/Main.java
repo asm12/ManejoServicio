0844f6ba5f5603a09dba18829b1ce980dfafd362
Albert <asm28@alumnes.udl.cat>
2021-04-18 13:53:07 +0200

    Added Toasts when selecting the action to do.

--- app/src/main/java/com/android/mdw/demo/Main.java
+++ app/src/main/java/com/android/mdw/demo/Main.java
@@ -6,43 +6,46 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Main extends Activity implements OnClickListener {
-  private Intent in;
+    private Intent in;
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
-    createButtons();
+        createButtons();
 
-    in = new Intent(this, CustomBroadcastReceiver.class);
-  }
-
-  private void createButtons() {
-    Button btnSonido = (Button) findViewById(R.id.boton_sonido);
-    Button btnCancion = (Button) findViewById(R.id.boton_cancion);
-    Button btnFin = (Button) findViewById(R.id.boton_fin);
-
-    btnSonido.setOnClickListener(this);
-    btnCancion.setOnClickListener(this);
-    btnFin.setOnClickListener(this);
-  }
-
-  @Override
-  public void onClick(View src) {
-    switch (src.getId()) {
-    case R.id.boton_sonido:
-      in.putExtra("action", R.string.iniciar_sonido);
-      break;
-    case R.id.boton_cancion:
-      in.putExtra("action", R.string.iniciar_cancion);
-      break;
-    case R.id.boton_fin:
-      in.putExtra("action", R.string.detener);
-      break;
+        in = new Intent(this, CustomBroadcastReceiver.class);
     }
-    sendBroadcast(in);
-  }
+
+    private void createButtons() {
+        Button btnSonido = (Button) findViewById(R.id.boton_sonido);
+        Button btnCancion = (Button) findViewById(R.id.boton_cancion);
+        Button btnFin = (Button) findViewById(R.id.boton_fin);
+
+        btnSonido.setOnClickListener(this);
+        btnCancion.setOnClickListener(this);
+        btnFin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View src) {
+        switch (src.getId()) {
+            case R.id.boton_sonido:
+                in.putExtra("action", getString(R.string.iniciar_sonido));
+                SingleToast.show(this, getString(R.string.toast_sonido), Toast.LENGTH_SHORT);
+                break;
+            case R.id.boton_cancion:
+                in.putExtra("action", getString(R.string.iniciar_cancion));
+                SingleToast.show(this, getString(R.string.toast_cancion), Toast.LENGTH_SHORT);
+                break;
+            case R.id.boton_fin:
+                in.putExtra("action", getString(R.string.detener));
+                break;
+        }
+        sendBroadcast(in);
+    }
 }
